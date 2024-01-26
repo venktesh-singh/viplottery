a3f142064d6f8073ad309f49511bf0736f51751d
Venktesh Singh <71117606+venktesh-singh@users.noreply.github.com>
2024-01-26 18:29:12 +0530

    First Commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '127.0.0.1:3306',
+    host: '127.0.0.1',
     user: 'u257446660_test',
     password: 'Test@@##$12',
-    database: 'u257446660_test'
+    database: 'u257446660_test',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
    // "start": "pm2 start src/server.js"
 
