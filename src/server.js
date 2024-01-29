4a303d1d9982942995bc8e4b94e4e1ac4553ba05
Venktesh Singh <71117606+venktesh-singh@users.noreply.github.com>
2024-01-29 16:22:04 +0530

    First Commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import configViewEngine from './config/configEngine';
+import configViewEngine from './config/configEngine.js';
 import routes from './routes/web';
 import cronJobContronler from './controllers/cronJobContronler';
 import socketIoController from './controllers/socketIoController';
