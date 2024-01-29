95933e56565e963785fd375a7aea41d956c4924b
Venktesh Singh <71117606+venktesh-singh@users.noreply.github.com>
2024-01-29 16:12:01 +0530

    First Commit

--- src/config/configEngine.js
+++ src/config/configEngine.js
@@ -1,9 +1,26 @@
 import express from 'express';
 
 const configViewEngine = (app) => {
+    // Serve static files from the 'public' directory
+    app.use(express.static("src/public"));
+
+    // Set the view engine to EJS
+    app.set('view engine', 'ejs');
+
+    // Set the views directory to 'views' inside the 'src' directory
+    app.set('views', 'src/views');
+};
+
+export default configViewEngine;
+
+
+
+/*import express from 'express';
+
+const configViewEngine = (app) => {
     app.use(express.static("./src/public"));
     app.set('view engine', "ejs");
     app.set('views', "./src/views");
 }
 
-export default configViewEngine;
+export default configViewEngine; */
